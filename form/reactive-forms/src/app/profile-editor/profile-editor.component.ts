34b907a605f25545446ce8037ea0e6b76f26f098
xinxiamu <932852117@qq.com>
2019-11-19 18:25:25 +0800

    提交

--- form/reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ form/reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -3,31 +3,37 @@ import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 
+/**
+ * FormGroup跟踪一组FormControl实例（比如一个表单）的表单状态。当创建 FormGroup 时，其中的每个控件都会根据其名字进行跟踪。
+ *
+ */
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
 export class ProfileEditorComponent {
-  profileForm = this.fb.group({
-    firstName: ['', Validators.required],
+  //profileForm是个嵌套表单,同时接纳 FormControl 和 FormGroup 作为子控件。
+  //来自内嵌控件组的状态和值的变更将会冒泡到它的父控件组，以维护整体模型的一致性。
+  profileForm = this.fb.group({//在视图中，profileForm是FormGroup的名称
+    firstName: ['', Validators.required],  //校验器要求必填。key是表单的名字，视图中的formControlName属性名称。必填字段必须都有值，表单状态才为可用状态，否则为不可用状态。
     lastName: [''],
-    address: this.fb.group({
+    address: this.fb.group({ //在视图中，address是formGroupName的名称
       street: [''],
       city: [''],
       state: [''],
       zip: ['']
     }),
-    aliases: this.fb.array([
+    aliases: this.fb.array([ //formArrayName
       this.fb.control('')
     ])
   });
 
   get aliases() {
-    return this.profileForm.get('aliases') as FormArray;
+    return this.profileForm.get('aliases') as FormArray; //表单组
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) { } //首先要引入表单组对象创建的对象。
 
 
   updateProfile() {
@@ -43,6 +49,8 @@ export class ProfileEditorComponent {
     this.aliases.push(this.fb.control(''));
   }
 
+  //form提交时间回调。
+  //使用 EventEmitter 向组件外部提供该表单的值
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
